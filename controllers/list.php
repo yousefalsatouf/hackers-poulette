ec30ab588dbd1f5bec729fd82654453c200d521d
Yousef Alsatouf <yousef.alsatouf94@gmail.com>
2019-11-05 11:11:32 +0100

    all done

--- controllers/list.php
+++ controllers/list.php
@@ -45,7 +45,7 @@ if (!empty($_POST)) {
     } else {
         $class_alert = "alert-success";
         $alert = "Success";
-        $show_msg = "Done, You can find It at the Top of the page!";
+        $show_msg = "Done, You can find It at the Topgit  of the page!";
         $data = array(
             "First Name: "  => $firstName,
             "Last Name: "   => $lastName,
